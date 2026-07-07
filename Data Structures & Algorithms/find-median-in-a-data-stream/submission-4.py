1f2559d8646944220746b1c8a1ee675783ad9d35
Leo Gretzinger <leogretz@gmail.com>
2026-07-07 09:04:48 -0700

    Add: find-median-in-a-data-stream - submission-4

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-4.py
@@ -0,0 +1,26 @@
+# intended two-heap approach without max-heap
+class MedianFinder:
+
+    def __init__(self):
+        # effective max heap by inserting all negatives
+        self.small= []
+        self.large = []
+        
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.small,-num)
+        # transfer max across, flip
+        heapq.heappush(self.large,-heapq.heappop(self.small))
+
+        if len(self.large) > len(self.small):
+            heapq.heappush(self.small,-heapq.heappop(self.large))
+        
+            
+    def findMedian(self) -> float:
+        if len(self.small) > len(self.large):
+            return -self.small[0]
+        return (-self.small[0] + self.large[0]) / 2
+
+        
+        
+        
+        
